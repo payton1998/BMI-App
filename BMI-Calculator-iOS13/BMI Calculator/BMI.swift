9b75a7da75768d252e8a080d2178d7fa2cc581cc
Payton Mitchell <payton016@gmail.com>
2024-07-11 14:19:19 -0400

    Add files via upload

---
+++ BMI-Calculator-iOS13/BMI Calculator/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Payton Mitchell on 6/16/24.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let pieceOfAdvice: String
+    let color: UIColor
+}
